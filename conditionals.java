df621c6099254c417a10218fa19143795db83b6f
kalpit jindal <67238343+kjjindal@users.noreply.github.com>
2021-08-04 09:20:43 +0530

    04-08-2021
    if.....else conditionals

---
+++ conditionals.java
@@ -0,0 +1,17 @@
+public class conditionals {
+
+    public static void main(String[] arg) {
+ 
+        int age=18;
+
+        // ==,>,>=,<,<=,!=    relational operator
+        if(age>=18){
+            System.out.println("Yes ,you are eligible for driving license");
+        }
+        else{
+            System.out.println("No,you are not eligible for driving license");
+        }
+        
+    }
+    
+}
