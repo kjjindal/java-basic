d9740c1dd5b3be796fd7cc1534df6c24ff1932dd
kalpit jindal <67238343+kjjindal@users.noreply.github.com>
2021-08-04 09:31:11 +0530

    4-08-2021
    conditionals and logical operators

--- conditionals.java
+++ conditionals.java
@@ -11,6 +11,45 @@ public class conditionals {
         else{
             System.out.println("No,you are not eligible for driving license");
         }
+
+
+        // logical operator
+        int age2=25;
+        String gender="male";
+         // and operator if both cases are true then it true otherwise it is false
+        if(age2>20 && gender=="male"){
+            System.out.println("yes, you can drive");
+
+        }
+        else{
+            System.out.println("no,you can not drive");
+        }
+
+         // or || operator if one of cases is true then it is true
+        if(age2>20 || gender=="female"){
+            System.out.println("yes, you can drive");
+
+        }
+        else{
+            System.out.println("no,you can not drive");
+        }
+         
+
+
+        // not logical operator convert true--> false and false ---> true
+
+        boolean a=true;
+        boolean b=false;
+
+        System.out.print("not of a is :");
+        System.out.println(!a);
+
+        System.out.print("not of b is :");
+        System.out.println(!b);
+
+
+
+
         
     }
     
