f723cc2cf300c54d1a537682629635156cb362f0
kalpit jindal <67238343+kjjindal@users.noreply.github.com>
2021-08-04 20:17:57 +0530

    4-08-2021
    Switch case in java

---
+++ switchcase.java
@@ -0,0 +1,23 @@
+public class switchcase {
+
+    public static void main(String[] args) {
+        int age = 18;
+
+        switch (age) {
+            case 18:
+                System.out.print("age is 18");
+
+                break;
+
+            case 20:
+                System.out.println("age is 20");
+
+            case 60:
+                System.out.println("age is 60");
+
+            default:
+                break;
+        }
+    }
+
+}
