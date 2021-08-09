c6313097c9e68010af778d90328817741b7aefd8
kalpit jindal <67238343+kjjindal@users.noreply.github.com>
2021-08-09 09:34:32 +0530

    9-08-2021
    while loop in java

---
+++ whileloop.java
@@ -0,0 +1,25 @@
+public class whileloop {
+
+    public static void main(String[] args) {
+
+        int i=0;
+
+        while(i<15){
+            System.out.println(i);
+            i++;
+        }
+
+        int j=0;
+        do{
+
+
+            System.out.println(j);
+            j++;
+
+
+
+        }while(j<20);
+        
+    }
+    
+}
