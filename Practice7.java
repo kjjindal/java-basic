386421d0c322ec54bcbf5f9b8b73e0a433942a31
kalpit jindal <67238343+kjjindal@users.noreply.github.com>
2021-08-10 09:37:02 +0530

    10-08-2021
    sum of muliplication table result

---
+++ Practice7.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+
+public class Practice7 {
+    public static void main(String[] args) {
+
+        int table_number=8;
+
+        int sum=0;
+
+        for(int i=1;i<=10;i++){
+            sum+=8*i;
+        }
+
+        System.out.println(sum);
+
+    
+        
+    }
+    
+}
