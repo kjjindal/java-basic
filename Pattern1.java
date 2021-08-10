17272782d2b9bc1b0626447c29ab06fc6f9d474d
kalpit jindal <67238343+kjjindal@users.noreply.github.com>
2021-08-10 08:46:31 +0530

    10-08-2021
    pattern 1

---
+++ Pattern1.java
@@ -0,0 +1,24 @@
+// * * * * *
+// * * * *
+// * * *
+// * * 
+// *
+
+
+public class Pattern1 {
+
+
+    public static void main(String[] args) {
+
+        int rows=5;
+
+        for(int i=0;i<rows;i++){
+            for(int j=5;j>i;j--){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+    
+}
